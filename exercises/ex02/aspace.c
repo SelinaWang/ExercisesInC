c73e8dd8cb788fb7c8898cecf46b9140297f010c
SelinaWang <ziyu.wang@students.olin.edu>
2018-02-08 14:57:30 -0500

    Addendum to HW2

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,17 +10,31 @@ License: GNU GPLv3
 
 int var1;
 
+void print_local()
+{
+  int local_var = 6;
+  printf ("Address of local var is %p\n", &local_var);
+}
+
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
+    void *q = malloc(128);
+    void *m = malloc(20);
+    void *n = malloc(20);
     char *s = "Literal string";
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("Address of p is %p\n", p);
+    printf ("Address of q is %p\n", q);
+    printf ("Address of m is %p\n", m);
+    printf ("Address of n is %p\n", n);
     printf ("Address of s is %p\n", s);
+
+    print_local();
 
     return 0;
 }
